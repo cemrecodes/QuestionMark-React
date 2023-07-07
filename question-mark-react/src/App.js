a87394f049e79811bdad1654fce536e2817a5011
cemrecodes <floracemre@hotmail.com>
2023-07-08 02:04:50 +0300

    feat auth

--- question-mark-react/src/App.js
+++ question-mark-react/src/App.js
@@ -1,20 +1,22 @@
 import './App.css';
-import React, { Component }  from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+import React  from 'react';
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'; 
 import Home from './components/Home/Home'
 import User from './components/User/User' 
 import Navbar from './components/Navbar/Navbar' 
+import Auth from './components/Auth/Auth';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar></Navbar>
-        <Routes>
-          <Route path="/" element = {<Home/>}></Route>
-          <Route path= "/users/:userId"  element = {<User/>}></Route>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users/:userId" element={<User />} />
+        <Route exact path="/auth" element={localStorage.getItem("currentUser") != null ? <Navigate to="/" /> : <Auth />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
